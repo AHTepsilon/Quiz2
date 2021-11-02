95b68b56623fbc0519f57857c08afe7c25afa015
AHTepsilon <alehertoro725@gmail.com>
2021-11-02 11:51:51 -0500

    added html, css, js and node files

---
+++ index.js
@@ -0,0 +1,5 @@
+const studentName = document.getElementById("studentTextField");
+const codeText = document.getElementById("codeTextField");
+const courseText = document.getElementById("course");
+
+const registerButton = document.getElementById("registerButton");
