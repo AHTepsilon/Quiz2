e7984215d233858639ce7ddf39e51280e5cf4ddf
AHTepsilon <alehertoro725@gmail.com>
2021-11-03 15:38:18 -0500

    added initial pattern to print stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "firebase/app";
-import {getDatabase, ref, set, onValue} from "firebase/database";
+import {getDatabase, ref, set, onValue, push} from "firebase/database";
 
 import {getFirebaseConfig} from "./firebase-config";
 
@@ -9,20 +9,49 @@ const firebaseApp = initializeApp(firebaseAppConfig);
 function registerUser(newUser)
 {
     const db = getDatabase();
-    const dbRef = ref(db, "user/ " + newUser.NAME);
-    set(dbRef, newUser);
+
+    const newUserRef = push(ref(db, "users"));
+    console.log(newUserRef);
+
+    //const dbRef = ref(db, "user/ " + newUser.NAME);
+    set(newUserRef, newUser);
 }
 
 function getUsers()
 {
     const db = getDatabase();
-    const dbRef = ref(db, "user");
+    const dbRef = ref(db, "users");
 
     onValue(dbRef, (snapshot) => 
     {
         const data = snapshot.val();
         console.log(data);
+        updateUsers(data);
     });
+}
+
+const userList1 = document.getElementById("bonusC1");
+
+function updateUsers(info)
+{
+    if(info)
+    {
+        let text = "";
+        Object.keys(info).forEach((key, index) =>
+        {
+            console.log(key, index);
+            text += info[key].NAME + "\n";
+        });
+
+        console.log(text)
+
+        //userList1.innerHTML(text);
+    }
+
+    else
+    {
+        //userList1.innerHTML("N/A");
+    }
 }
 
 const studentName = document.getElementById("studentTextField");
@@ -33,7 +62,7 @@ const registerButton = document.getElementById("registerButton");
 
 function test()
 {
-    alert("hello world")
+    alert("hello world");
 }
 
 const saveUserData = (e, event) =>
@@ -50,6 +79,9 @@ const saveUserData = (e, event) =>
     };
 
     registerUser(newUser);
+
+    alert("Usuario registrado exitosamente");
 }
 
 registerButton.addEventListener("click", saveUserData);
+registerButton.addEventListener("click", getUsers);
